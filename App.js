fb44ab00aacd60a98fad81077c202ad755b070e8
Matheus Allan <matheusrpd@gmail.com>
2020-03-13 19:09:47 -0300

    Add editorconfig, prettier e eslint.

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text, StatusBar } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <StatusBar barStyle="dark-content" />
+    <Text>Hello Matheus</Text>
+  </SafeAreaView>
+);
+
+export default App;
