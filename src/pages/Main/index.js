c99fb89f25b0e7a86b8d3520192fe71be9b0a069
Matheus Allan <matheusrpd@gmail.com>
2020-03-19 17:58:16 -0300

    Guardando e buscando os users no storage.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -21,6 +22,26 @@ export default function Main() {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState('');
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const usersStorage = await AsyncStorage.getItem('users');
+
+      if (usersStorage) {
+        setUsers(JSON.parse(usersStorage));
+      }
+    };
+
+    loadUsers();
+  }, []);
+
+  useEffect(() => {
+    const saveUsers = () => {
+      AsyncStorage.setItem('users', JSON.stringify(users));
+    };
+
+    saveUsers();
+  }, [users]);
 
   const handleSubmit = async () => {
     setLoading(true);
