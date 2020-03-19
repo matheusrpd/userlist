f4dc42030cacfaa8c9be6f1c0ae063f0e6538418
Matheus Allan <matheusrpd@gmail.com>
2020-03-19 17:25:40 -0300

    Add visualizaçao de usuario na tela main.

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -33,3 +33,56 @@ export const SubmitButton = styled(RectButton)`
   height: 40px;
   padding: 0 12px;
 `;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const User = styled.View`
+  align-items: center;
+  margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+  height: 64px;
+  width: 64px;
+  border-radius: 32px;
+  background: #eee;
+`;
+
+export const Name = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  color: #333;
+  margin-top: 4px;
+`;
+
+export const Bio = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 13px;
+  line-height: 18px;
+  text-align: center;
+  color: #999;
+  margin-top: 5px;
+`;
+
+export const ProfileButton = styled(RectButton)`
+  margin-top: 10px;
+  align-self: stretch;
+  height: 36px;
+  border-radius: 4px;
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProfileButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #fff;
+`;
