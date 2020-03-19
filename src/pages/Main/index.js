d86572dac4205aab6e50f40e40f8be0847f1f969
Matheus Allan <matheusrpd@gmail.com>
2020-03-19 17:04:47 -0300

    Add form na pagina main e buscando dados na api.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Text>Main</Text>;
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  const handleSubmit = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const user = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, user]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+        />
+        <SubmitButton onPress={handleSubmit}>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
